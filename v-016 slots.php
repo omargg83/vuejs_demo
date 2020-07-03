2935866269495b4e0b2660f40de9e23af11dc705
Omar <51249360+omargg83@users.noreply.github.com>
2020-07-03 14:40:37 -0500

    a

---
+++ v-016 slots.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hola mundo</title>
+
+  </head>
+  <body>
+    <main id='app'>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script>
+      const app = new Vue({
+        el: '#app',
+        data:{
+        datos:""
+        }
+      })
+    </script>
+
+  </body>
+</html>
